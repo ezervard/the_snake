db43ae0e37394ef429e05cf91532d28f6a77b8bf
Romes <ezervard@gmail.com>
2024-05-16 19:26:50 +0300

    refactoring

---
+++ Moduls/CONSTANTS.py
@@ -0,0 +1,59 @@
+import pygame
+from random import choice
+
+
+pygame.init()
+
+# Константы для размеров поля и сетки:
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+GRID_SIZE = 20
+GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+CENTER_POSITION = SCREEN_WIDTH // 2 - GRID_SIZE, SCREEN_HEIGHT // 2 - GRID_SIZE
+
+# Направления движения:
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
+DEFAULT_DIRECTION = choice(DIRECTIONS)
+
+# Цвет фона:
+BOARD_BACKGROUND_COLOR = (108, 156, 31)
+BACKGROUND_IMG = pygame.image.load('./images/background.png')
+BACKGROUND_IMG = pygame.transform.scale(BACKGROUND_IMG, (640, 490))
+
+# Игровые звуки
+BACKGROUND_MUSIC = pygame.mixer.Sound('./sounds/main_theme.mp3')
+APPLE_SOUND = pygame.mixer.Sound('./sounds/apple.mp3')
+SELF_EAT_SOUND = pygame.mixer.Sound('./sounds/self_eat.mp3')
+CHOOSE_SOUND = pygame.mixer.Sound('./sounds/key_sound.mp3')
+
+# Цвет границы ячеек
+BORDER_COLOR = (93, 216, 228)
+
+# Цвет по умолчанию
+DEFAULT_COLOR = (100, 100, 100)
+
+# Цвет яблока
+APPLE_COLOR = (255, 0, 0)
+
+# Цвет змейки
+SNAKE_COLOR = (0, 255, 0)
+
+# Скорость движения змейки:
+SPEED = 10
+
+# Настройка игрового окна:
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT + 30), 0, 32)
+
+# Переменная состояний
+STATE = "game_play"
+
+# Заголовок окна игрового поля:
+pygame.display.set_caption('Змейка')
+
+# Настройка времени:
+clock = pygame.time.Clock()
---
+++ Moduls/handlers_key.py
@@ -0,0 +1,50 @@
+import pygame
+
+from moduls import CONSTANTS as ct
+
+
+def handle_keys(game_object) -> None:
+    """Функция для обработки нажатий клавиш"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_game()
+        elif event.type == pygame.KEYDOWN:
+            handle_direction(event, game_object)
+            handle_special_keys(event)
+
+
+def handle_direction(event, game_object) -> None:
+    """Метод описывающий изменение направления"""
+    if event.key == pygame.K_UP and game_object.direction != ct.DOWN:
+        game_object.next_direction = ct.UP
+    elif event.key == pygame.K_DOWN and game_object.direction != ct.UP:
+        game_object.next_direction = ct.DOWN
+    elif event.key == pygame.K_LEFT and game_object.direction != ct.RIGHT:
+        game_object.next_direction = ct.LEFT
+    elif event.key == pygame.K_RIGHT and game_object.direction != ct.LEFT:
+        game_object.next_direction = ct.RIGHT
+
+
+def handle_special_keys(event) -> None:
+    """Обработка специальных клавиш"""
+    if event.key == pygame.K_ESCAPE:
+        quit_game()
+    elif event.key == pygame.K_SPACE:
+        ct.CHOOSE_SOUND.play()
+        state_change()
+
+
+def quit_game() -> None:
+    """Выход из игры"""
+    pygame.quit()
+    raise SystemExit
+
+
+def state_change() -> None:
+    """Изменение переменной состояния"""
+    if ct.STATE == "game_play":
+        ct.STATE = "game_pause"
+    elif ct.STATE == "game_pause":
+        ct.STATE = "game_play"
+    elif ct.STATE == "game_over":
+        ct.STATE = "game_play"
